dd7ed52104906cb216a1403c6bde054e14c4e179
Forests <99336909+forests0@users.noreply.github.com>
2022-07-17 11:29:21 +0900

    gif 올리기

---
+++ Function/gif.js
@@ -0,0 +1,42 @@
+const discord = require("discord.js");
+const config = require("./config.json");
+const client = new discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const Attachment = require('discord.js');
+const prefix = "!";
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("Just", { type: "WATCHING" });
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix)) return;
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(" ");
+  const command = args.shift().toLowerCase();
+
+  switch (command) {
+    case "help":
+      message.reply("It will help you....maybe");
+      break;
+    case "gif":
+      const args = message.content.split(" ").slice(1);
+      var no = args.join(" ");
+      if (!no) no = "1";
+      if (no == 2) {
+        return message.reply("sorry.. this gif is too big. so I can\'t upload.");
+        break;
+      }
+      if (isNaN(no) || no > 14) return message.reply("only number 1-14");
+      var path = "./gif/";
+      path = path.concat(no, ".gif");
+      console.log(path);
+      const attachment = new discord.MessageAttachment(path);
+      message.channel.send(attachment);
+      break;
+    default:
+      message.reply("It's not a funciton");
+  }
+});
+
+client.login(config.BOT_TOKEN);
