f6ef179e378e5fc0a63b63736cf43c5c1ddeeb2c
Forests <99336909+forests0@users.noreply.github.com>
2022-07-21 18:19:25 +0900

    코드 수정

--- Function/welcome.js
+++ Function/welcome.js
@@ -2,12 +2,12 @@ const discord = require("discord.js");
 const config = require("./config.json");
 const client = new discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
-bot.on('guildMemberAdd', member) = () => {
+bot.on('guildMemberAdd', (member) => {
     const channel = member.guild.channels.find(channel => channel.name === "welcome");
     if(!channel) return;
     channel.send(
         `Welcome to our server, ${member}! When you type !help, You can see the embed!`
         )
-}
+});
 
 client.login(config.BOT_TOKEN);
