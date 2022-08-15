9b8569b1162dbd4269204fe35991cece6d9fb2db
Forests <99336909+forests0@users.noreply.github.com>
2022-08-16 08:00:37 +0900

    welcome.js 수정

--- Function/welcome.js
+++ Function/welcome.js
@@ -2,12 +2,19 @@ const discord = require("discord.js");
 const config = require("./config.json");
 const client = new discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
-bot.on('guildMemberAdd', (member) => {
-    const channel = member.guild.channels.find(channel => channel.name === "welcome");
-    if(!channel) return;
-    channel.send(
-        `Welcome to our server, ${member}! When you type !help, You can see the embed!`
-        )
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("Just", { type: "WATCHING" });
+});
+
+bot.on("guildMemberAdd", (member) => {
+  const channel = member.guild.channels.find(
+    (channel) => channel.name === "welcome"
+  );
+  if (!channel) return;
+  channel.send(
+    `Welcome to our server, @${member}! When you type !help, You can see the embed!`
+  );
 });
 
 client.login(config.BOT_TOKEN);
