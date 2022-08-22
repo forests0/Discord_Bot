771058b56ecdc38a01b7b0ad6d579a04c94848ab
Forests <99336909+forests0@users.noreply.github.com>
2022-08-23 08:00:41 +0900

    역할얻기 수정

--- Function/commands/getroles.js
+++ Function/commands/getroles.js
@@ -1,17 +1,27 @@
 module.exports = {
   name: "getroles",
   execute(msg) {
-    const commandBody = msg.content.slice(prefix.length);
-    const command = args.shift().toLowerCase();
     const args = msg.content.split(" ").slice(1);
     var roles = args.join(" ");
-    if (!roles) msg.reply('Write the roles. I don\'t know what do you want!');
+    if (!roles) msg.reply('Write the roles. If you don\'t know what is the roles, type \'!getroles help\'.');
     switch(roles) {
+      case 'help' :
+        msg.channel.send('roles : basic | intermediate | advanced || You type !getroles (rolename)')
+        break;
       case 'basic' :
         msg.member.roles.add("1009659511717494854");
+        msg.reply("설정 완료!");
+        break;
+      case 'intermediate' :
+        msg.member.roles.add('1011077927925989426');
+        msg.reply("설정 완료!");
+        break;
+      case 'advanced' :
+        msg.member.roles.add('1011079971088896191');
+        msg.reply("설정 완료!");
         break;
     }
     //msg.member.setNickname(`✓${msg.member.user.username}`);
-    msg.reply("설정 완료!");
+    
   },
 };
