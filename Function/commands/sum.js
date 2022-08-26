aa5167727169671a32c987a0d42a0dc81c2d3d32
Forests <99336909+forests0@users.noreply.github.com>
2022-08-27 00:06:56 +0900

    sum

---
+++ Function/commands/sum.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "sum",
+  execute(msg) {
+    const prefix = "!";
+    const commandBody = msg.content.slice(prefix.length);
+    const args = commandBody.split(" ");
+
+    const numArgs = args.map((x) => parseFloat(x));
+    const sum = numArgs.reduce((counter, x) => (counter += x));
+    msg.channel.send(`합계 : ${sum}`);
+    //뭔 이유인지 모르겠는데 sum 변수가 제 일을 하지 못하는 중, 수정해야됨
+  },
+};
