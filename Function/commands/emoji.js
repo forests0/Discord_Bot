357c297c402ce60a22bc913e3964a3da560c23ca
Forests <99336909+forests0@users.noreply.github.com>
2022-09-24 00:04:43 +0900

    emoji 문제수정

--- Function/commands/emoji.js
+++ Function/commands/emoji.js
@@ -11,6 +11,7 @@ module.exports = {
         "https://mblogthumb-phinf.pstatic.net/MjAxOTEwMTVfMTE2/MDAxNTcxMTQ0MjIwODE4.Yd4D6HvDtIQ600HpJJJbadGBT8aVJ91nQ4OUp6jd7oYg.uvrak7O9UOuiIKfWDh7KG906yeEYBSptki2qdXw__YMg.PNG.azzi_01/01_.png?type=w800"
       );
       msg.channel.send(emoji);
+      return ;
     }
     if (isNaN(no) || no > 8) return msg.reply("only number 1-8");
     var path = "./emoji/";
